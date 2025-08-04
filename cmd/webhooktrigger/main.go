e551ef7de6544b63f5a6775b0176e6a970b64efe
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 20:38:10 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/webhooktrigger/main.go
@@ -0,0 +1,23 @@
+// cmd/webhooktrigger/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"webhooktrigger/internal/webhooktrigger"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := webhooktrigger.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
